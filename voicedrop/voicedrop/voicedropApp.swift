b5d401062a70b5e10b65ca83396631ca1899e5be
crzyc <crzyc98@gmail.com>
2026-05-24 16:15:41 -0400

    add initial Xcode project structure

---
+++ voicedrop/voicedrop/voicedropApp.swift
@@ -0,0 +1,17 @@
+//
+//  voicedropApp.swift
+//  voicedrop
+//
+//  Created by Nicholas Amaral on 5/24/26.
+//
+
+import SwiftUI
+
+@main
+struct voicedropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
